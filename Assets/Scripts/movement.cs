4ace2a7d58ad966d20486743520af9a2351cd310
Craig Bauer <craigbauer23@gmail.com>
2023-01-13 16:29:58 -0500

    init commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] float mianThrust = 1000f;
+    [SerializeField] float rotationThrust = 50f;
+    Rigidbody rb;
+
+    // member var = global in this space
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //get reference to rigidBody
+    }
+
+    // Update is called once per frame
+    void Update() {
+        processThrust();
+        processRotation();
+    }
+
+    void processThrust() {
+        Debug.Log("UP");
+        if(Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * mianThrust * Time.deltaTime); //0, 1, 0 <-same;
+        }
+    }
+
+    void processRotation() {
+        if(Input.GetKey(KeyCode.A)) {
+            Debug.Log("Rotating Left");
+            ApplyRotation(rotationThrust);
+            //transform.Rotate(Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            Debug.Log("Rotating Right");
+            ApplyRotation(-rotationThrust);
+            //transform.Rotate(-Vector3.forward * rotationThrust * Time.deltaTime);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame) {
+        rb.freezeRotation = true; //freezing physics system
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; //un-freezing physics system
+    }   
+}
